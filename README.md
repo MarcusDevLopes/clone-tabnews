1ea3cfd46e3ac6be10e1fa869a1934d294badf6d
MarcusDevLopes <dev.marcuslopes@gmail.com>
2025-05-28 23:56:34 +0000

    Adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 iniciando nesse novo mundo e implementar o https://www.tabnews.com.br para o https://curso.dev ;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é o maior amor da minha vida ! Eu te amo!! S2 S2 S2</h1>
+  return <h1>Você é o maior amor da minha vida ! Eu te amo!! S2 S2 S2</h1>;
 }
 
-export default Home
+export default Home;
